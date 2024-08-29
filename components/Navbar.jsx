ceb92da65c1c60e884145974f4f173505f3e3609
fairleyv <fairleyvincent@gmail.com>
2024-08-29 16:53:07 -0700

    created the structure for the NavBar Component and removed the useState import from the component.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,5 +2,20 @@ import React from "react"
 import logo from "../src/assets/readmeMakerLogo.jpg"
 
 export default function Navbar () {
-    return <h1>hello</h1>
+    return (
+    <nav>
+        <div className="navbar--title--section">
+            <img className="navbar--logo" src={logo} width="50px"/>
+            <h1 className="navbar--title">Readme Maker</h1>
+        </div>
+
+        <div className="navbar--links--section">
+            <h3 className="navbar--link">How To Use</h3>
+            <h3 className="navbar--link">ReadMe Maker</h3>
+            <h3 className="navbar--link">Download Button</h3>
+            <h3 className="navbar--link">Footer</h3>
+        </div>
+
+    </nav>
+    )
 }
