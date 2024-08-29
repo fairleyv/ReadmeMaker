d624d0ffa4e2e011d954656660c405759bb4c83f
fairleyv <fairleyvincent@gmail.com>
2024-08-29 16:23:20 -0700

    added a Footer component to the components folder and imported all components to the App.jsx file. Also added the componetns to the app function in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
 import './App.css'
+import Navbar from "../components/Navbar"
+import HowtoUse from "../components/HowtoUse"
+import ReadmeForm from "../components/ReadmeForm"
+import DownloadButton from "../components/DownloadButton"
+import Markup from "../components/Markup"
+import Footer from "../components/Footer"
 
 function App() {
+    <>
+        <Navbar />
+        <HowtoUse />
+        <div className="readme--output">
+            <ReadmeForm />
+            <Markup />
+        </div>
+        <DownloadButton />
+        <Footer />
+    </>
+
   
 }
 
