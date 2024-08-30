9bc1d38fe98b974361ee89ad70212fd21707f346
fairleyv <fairleyvincent@gmail.com>
2024-08-30 08:54:44 -0700

    added renderLicenseBadge function to Readmeform

--- components/Readmeform.jsx
+++ components/Readmeform.jsx
@@ -2,5 +2,18 @@ import React from "react"
 import {useState} from "react"
 
 export default function ReadmeForm () {
-    return 
+    function renderLicenseBadge(license) {
+        if (license == 'N/A') {
+            return "" 
+        } else if (license == 'MIT') {
+            return `[![License](https://img.shields.io/badge/License-MIT-yellow.svg)]`
+        } else {
+            return `[![License](https://img.shields.io/badge/License-${license}-blue.svg)]`
+        }
+    }
+    return (
+        <form>
+
+        </form>
+    )
 }
