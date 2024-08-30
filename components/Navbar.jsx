7a30cc6b6feb14e8a814890ac69b19637886fc17
fairleyv <fairleyvincent@gmail.com>
2024-08-29 17:14:10 -0700

    added css to the App.css file to better organize the navbar. Also added anchor tags to the elements under the navbar links section className

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,17 +3,17 @@ import logo from "../src/assets/readmeMakerLogo.jpg"
 
 export default function Navbar () {
     return (
-    <nav>
+    <nav className="navbar">
         <div className="navbar--title--section">
             <img className="navbar--logo" src={logo} width="50px"/>
             <h1 className="navbar--title">Readme Maker</h1>
         </div>
 
         <div className="navbar--links--section">
-            <h3 className="navbar--link">How To Use</h3>
-            <h3 className="navbar--link">ReadMe Maker</h3>
-            <h3 className="navbar--link">Download Button</h3>
-            <h3 className="navbar--link">Footer</h3>
+            <h3 className="navbar--link"><a href="#">How To Use</a></h3>
+            <h3 className="navbar--link"><a href="#">ReadMe Maker</a></h3>
+            <h3 className="navbar--link"><a href="#">Download Button</a></h3>
+            <h3 className="navbar--link"><a href="#">Footer</a></h3>
         </div>
 
     </nav>
