dd5dcf7afdb7d18c7dc48ed37eb6e0eb43a357d8
fairleyv <fairleyvincent@gmail.com>
2024-09-07 21:00:40 -0700

    refactored markup so that the markup is separated into three separate arrays that are mapped over and called in the return statement.

--- components/ReadmeMarkup.jsx
+++ components/ReadmeMarkup.jsx
@@ -3,92 +3,65 @@ import {useState} from "react"
 
 export default function ReadmeMarkup (props) {
 
-    const tableOfContentsArr = [
-         "## Table of Contents\n",
-        "[Description](#description)\n",
-        "[Installation](#installation)\n",
-        "[Usage](#usage)\n",
-        "[Credits](#credits)\n",
-        "[License](#license)\n",
-        "[Contributions](#contributions)\n",
-        "[Tests](#tests)\n",
-        "[Questions](#questions)\n",
+    const tableOfContentsArr = props.includeTableofContents? [
+         "## Table of Contents\\n",
+        "[Description](#description)\\n",
+        "[Installation](#installation)\\n",
+        "[Usage](#usage)\\n",
+        "[Credits](#credits)\\n",
+        "[License](#license)\\n",
+        "[Contributions](#contributions)\\n",
+        "[Tests](#tests)\\n",
+        "[Questions](#questions)\\n",
+    ] :  [
+        "## Table of Contents\\n ",
+        "N/A \\n"
     ]
 
-    const formattedTableofContentsArr = tableOfContentsArr.map (item => <pre>{item}</pre>)
+    const aboveTableofContentsArr = [
+        `${props.licenseBadge}${props.licenseLink}\\n`,
+        `# ${props.title} \\n`,
+        "## Description \\n",
+        `\\t ${props.description} \\n`
+
+    ]
+
+    const belowTableofContentsArr = [
+        "## installation \\n",
+        `${props.installation} \\n`,
+        "##usage", 
+        `${props.usage} \\n`,
+        "## Credits \\n",
+        `${props.credits} \\n`,
+        "## License \\n",
+        props.license ? `This project is covered under the ${props.license} \\n` : `N/A \\n`,
+        "## Contributions \\n", 
+        `${props.contributions} \\n`,
+        "## Tests \\n",
+        `${props.tests} \\n`,
+        "## Questions \\n",
+        "For any questions regarding this project please contact me at my -\\n",
+        `GitHub: [${props.githubUsername}](https://github.com/${props.githubUsername}) \\n`,
+        `Email: ${props.email}`
+
+    ]
+
+    const formattedAboveTableofContentsArr = aboveTableofContentsArr.map(item=> <h4>{item}</h4>)
+    const formattedTableofContentsArr = tableOfContentsArr.map (item => <h4>{item}</h4>)
+    const formattedBelowTableofContentsArr = belowTableofContentsArr.map (item => <h4>{item}</h4>)
 
     return (
         <div className="readme--markup">
-            {/* // ${props.licensebadge}${props.licenseLink}\n */}
-                <pre>
-                    {`# ${props.title}`} \n
-                    </pre>
-
-                <pre>
-                    {`## Description`} \n
-                </pre>
-
-                <pre>
-                    \t{` ${props.description}`} \n
-                </pre>
-
-            {props.includeTableofContents && 
+            <div>
+                {formattedAboveTableofContentsArr}
+            </div>
             <div>
                 {formattedTableofContentsArr}
             </div>
-            } 
-            {!props.includeTableofContents && 
-
-                <pre> 
-                    `## Table of Contents\n 
-                    N/A\n`
-                </pre>
-                }
-
-                <pre>
-                ## Installation\n
-                </pre>
-                <pre>
-                {props.installation} \n
-                </pre>
-                <pre>
-                ## Usage \n
-                </pre>
-                <pre>
-                {props.usage} \n
-                </pre>
-                <pre>
-                ## Credits \n
-                </pre>
-                <pre>
-                {props.credits} \n
-                </pre>
-                <pre>
-                ## License \n
-                </pre>
-                <pre>
-                {props.license ? `This project is covered under the ${props.license}` : `N/A`} \n 
-                </pre>
-                <pre>
-                ## Contributions \n
-                </pre>
-                <pre>
-                {props.contributions} \n
-                </pre>
-                <pre>
-                ## Tests \n
-                </pre>
-                <pre>
-                {props.tests}\n
-                </pre>
-                <pre>
-                ## Questions \n
-                </pre>
-                <pre>
-                For any questions regarding this project please contact me at my -\n
-                GitHub: [{props.username}]`(https://github.com/${props.githubUsername})` \n
-                Email: {props.email}
-                </pre>
+            
+            <div>
+                {formattedBelowTableofContentsArr}
+            </div>
         </div>
     )
 }
